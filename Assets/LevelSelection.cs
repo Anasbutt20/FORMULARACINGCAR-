ad0da133f49ca5c7fefaf05d9299e6758cd2bc5a
anas butt <anasb5988@gamil.com>
2022-09-23 12:23:59 +0500

    update

--- Assets/LevelSelection.cs
+++ Assets/LevelSelection.cs
@@ -35,10 +35,10 @@ public class LevelSelection : MonoBehaviour
             PlayerPrefs.SetInt("levelunlocked",0);
         }
     }
-     public async void loadscene(int index)
+     public async void loadscene(string scenename)
     {
-        levelNum = index;
-       // SceneManager.LoadScene(index);
+       
+        // SceneManager.LoadScene(index);
         //var scene = SceneManager.LoadSceneAsync(1);
         //scene.allowSceneActivation = false;
         //_loadercanvas.SetActive(true);
@@ -54,8 +54,15 @@ public class LevelSelection : MonoBehaviour
 
         //scene.allowSceneActivation = true;
         //_loadercanvas.SetActive(false);
-
-         SceneManager.LoadScene(1);
+        List<string> splitString = new List<string>();
+        string[] bb;
+        bb = scenename.Split(' ');
+        splitString.Add(bb[0]);
+        splitString.Add(bb[1]);
+        LevelSelection.levelNum = int.Parse(splitString[1]);
+        string deciderString = splitString[0];
+        SceneManager.LoadScene(deciderString);
+       
 
     }
 
